536418702a4173af9473e5ef902e0c4ef3e6ec8a
Connor-Corbin <Connor.M.Corbin-1@ou.edu>
2023-09-13 15:59:10 -0500

    added main method, fixed product method

--- assignment3.py
+++ assignment3.py
@@ -5,7 +5,18 @@ def ProductList(list):
     if len(list) == 0:
             return 0
     total = 1
-    for i in list:
+    for i in range(len(list)):
         total = total * list[i]
     return total
 
+def main():
+    x = input("Please enter a list of integers seperated by a space ")
+    y = x.split(" ")
+    for i in range(len(y)):
+        y[i] = int(y[i])
+    print(y)
+    print("The sum of the list is: " + str(ComputeList(y)))
+    print("The product of the list is: " + str(ProductList(y)))
+
+if __name__ == "__main__":
+    main()
