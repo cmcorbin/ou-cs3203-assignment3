4e5b4d036dca88ffaee94d6c1ae01b25e98ea576
Connor-Corbin <Connor.M.Corbin-1@ou.edu>
2023-09-13 15:31:51 -0500

    added product list function

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,11 @@
 def ComputeList(list):
     return sum(list)
 
+def ProductList(list):
+    if len(list) == 0:
+            return 0
+    total = 1
+    for i in list:
+        total = total * list[i]
+    return total
+
